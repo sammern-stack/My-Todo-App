2a90494d85704899564f65325a2bfa1ddc75c08d
sammern-stack <samreactdev@gmail.com>
2026-05-07 18:31:15 +0300

    feat(server): initialize server with Express, MongoDB, and TypeScript setup
    - Added package.json for server dependencies and scripts
    - Created server.ts to set up Express app, connect to MongoDB, and enable CORS
    - Added tsconfig.json for TypeScript configuration with strict type checking and module settings

---
+++ server/server.ts
@@ -0,0 +1,32 @@
+// Import Required Modules
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+// Load Environmental Variables
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+const MONGO_URL = process.env.MONGO_URL;
+
+// Enable CORS
+const corsConfig = require("./config/cors.json");
+
+// Initialize Express App
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cors(corsConfig));
+
+// Connect to MongoDB and start the server
+mongoose
+  .connect(MONGO_URL)
+  .then(() => {
+    console.log(`Successfully connected to Mongodb`);
+
+    app.listen(PORT, () => {
+      console.log("server started");
+    });
+  })
+  .catch((err: any) => console.log(err));
